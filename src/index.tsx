1c2b683e4505974cb4f81a3cc5e9df7183f61377
Andrew Kim <107439239+i30101@users.noreply.github.com>
2024-09-12 10:56:55 -0400

    temp

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,35 @@
+/**
+ * @author Andrew Kim
+ * @version 0.0.1
+ * @since 12 September 2024
+ * 
+ * Index page
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// styling
+import './css/index.css';
+
+
+
+// Classes
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Navbar from './Navbar'
+
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Navbar />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
