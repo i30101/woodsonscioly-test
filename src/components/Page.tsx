7beb8eacd066ffc015c82a940184f419e915780f
i30101 <andrewkim229@gmail.com>
2024-09-16 10:53:00 -0400

    more refactoring

---
+++ src/components/Page.tsx
@@ -0,0 +1,38 @@
+/**
+ * @author Andrew Kim
+ * @version 0.0.1
+ * @since 2 December 2024
+ * 
+ * Page template
+ */
+
+
+import React from "react";
+
+import Navbar from "./Navbar";
+import Title from "./Title";
+import Body from "./Body";
+import Footer from "./Footer";
+
+
+interface PageProps {
+    children: React.ReactNode;
+    title: string;
+    highlight: string;
+    after: string;
+    subtitle: string;
+}
+
+const Page: React.FC<PageProps> = ({children, title, highlight, after, subtitle}) => (
+    <>
+        <Navbar />
+        <Title title={title} highlight={highlight} after={after} subtitle={subtitle} />
+        <Body>
+            {children}
+        </Body>
+        <Footer />
+    </>
+);
+
+
+export default Page;
