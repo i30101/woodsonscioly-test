89bdac6d6278bb97fd7a7314df624885e0bceae4
i30101 <andrewkim229@gmail.com>
2024-11-26 08:22:06 -0500

    blog pages

---
+++ src/components/BlogPage.tsx
@@ -0,0 +1,33 @@
+/**
+ * @author Andrew Kim
+ * @version 0.0.1
+ * @since 22 November 2024
+ * 
+ * Blog container
+ */
+
+
+import React from 'react';
+
+
+// styling
+import '../css/BlogPage.css'
+
+
+interface BlogProps {
+    title: string;
+    date: string;
+    author: string;
+    image: any;
+    category: string;
+}
+
+
+const BlogPage: React.FC<BlogProps> = ({title, date, author, image, category}) => {
+    return <>
+        <div className="intro-container"></div>
+    </>
+}
+
+
+export default BlogPage;
